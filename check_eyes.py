f80c9f8d379ace5d5e7f6055bdd634b5f1bc2f57
Ana Beatriz Freires Ferreira <30874488+anabeatrizfreires@users.noreply.github.com>
2020-10-20 19:06:48 -0300

    Update check_eyes.py

--- check_eyes.py
+++ check_eyes.py
@@ -8,6 +8,30 @@ from imutils.video import VideoStream
 import datetime
 import argparse
 import imutils
+import threading
+
+def picam_thread(picam):
+    # capture frames from the camera
+    for frame_1 in picam.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        # representacao da imagem com raw truncate
+        image1 = frame_1.array
+        frame2 = cv2.cvtColor(image1, cv2.COLOR_BGR2YUV)
+        frame2[:,:,0] = cv2.equalizeHist(frame2[:,:,0])
+        image = cv2.cvtColor(frame2, cv2.COLOR_YUV2BGR)
+        # Converte o frame para escala de cinza para poder trabalhar no 'haarcascade'
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        canvas = detect(gray, image)
+ 
+        # show the frame
+        cv2.imshow("Camera_Olho", image)
+        key = cv2.waitKey(1) & 0xFF
+ 
+        # limpa o stream p proximo frame
+        rawCapture.truncate(0)
+ 
+        # se q é pressionado fecha janela
+        if key == ord("q"):
+            break
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eyes_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
@@ -50,16 +74,14 @@ time.sleep(2.0)
 picam.resolution = (640, 480)
 picam.framerate = 32
 rawCapture = PiRGBArray(picam, size=(640, 480))
- 
-# allow the camera to warmup
+
+picam_t = threading.Thread(target=picam_thread, args=(picam,))
+picam_t.start()
+
 time.sleep(0.1)
- 
+
 while True:
-    # grab the frame_1 from the threaded video stream and resize it
-    # to have a maximum width of 400 pixels
-    #frame_1 = picam.read()
     frame_2 = webcam.read()
-    #frame_1 = imutils.resize(frame_1, width=400)
     frame_2 = imutils.resize(frame_2, width=400)
 
     # draw the timestamp on the frame_1
@@ -70,33 +92,13 @@ while True:
     cv2.putText(frame_2, ts, (10, frame_2.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
         0.35, (0, 0, 255), 1)
 
-    cv2.imshow("webcam", frame_2)
+    cv2.imshow("Camera_Cena", frame_2)
     key = cv2.waitKey(1) & 0xFF
 
     if key == ord("b"):
         break
 
-# capture frames from the camera
-for frame_1 in picam.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    # representacao da imagem com raw truncate
-    image1 = frame_1.array
-    frame2 = cv2.cvtColor(image1, cv2.COLOR_BGR2YUV)
-    frame2[:,:,0] = cv2.equalizeHist(frame2[:,:,0])
-    image = cv2.cvtColor(frame2, cv2.COLOR_YUV2BGR)
-  # Converte o frame para escala de cinza para poder trabalhar no 'haarcascade'
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    canvas = detect(gray, image)
- 
-    # show the frame
-    cv2.imshow("Frame", image)
-    key = cv2.waitKey(1) & 0xFF
- 
-    # limpa o stream p proximo frame
-    rawCapture.truncate(0)
- 
-    # se q é pressionado fecha janela
-    if key == ord("q"):
-        break
+
 cv2.destroyAllWindows()
 picam.stop()
 webcam.stop()
