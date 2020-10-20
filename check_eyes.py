870dd8573d6bb9a752dfa9c31fa60116481d3366
Ana Beatriz Freires Ferreira <30874488+anabeatrizfreires@users.noreply.github.com>
2020-10-20 18:13:24 -0300

    Update check_eyes.py

--- check_eyes.py
+++ check_eyes.py
@@ -1,59 +1,102 @@
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
 import cv2
+import numpy as np
+from imutils.video import VideoStream
+import datetime
+import argparse
+import imutils
 
-# entra com as 'haarcascades'
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eyes_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-# Define a função que fará a detecção 
-def detect(gray, frame):
-   #Input = frame do vídeo
-   #Output = frame com o retangulo demarcado
-  
-  # detecta os rostos usando o 'haarcascade'
-  
+def detect(gray, image):
+    
   faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-  
-  #saídas:
-  # x,y => coordenadas detectadas da face
-  # width(w) do retangulo no rosto
-  # height(h) do retangulo no rosto
-
-  #parametros de entrada:
-  # gray é a imagem de entrada (frame)
-  # 1.3 é o tamanho do kernel usado para detecção
-  # 5 é um valor de robustes na detecção do rosto, diminui os falsos positivos
-  
-  # Iteração para detecção continuada dos rostos
-  for (x,y,w,h) in faces:
-    cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
-    #parâmetros de entrada: imagem, coordenadas superiores, coordenadas laterais, cor,         #rectangle border thickness
-    
-    # Definimos duas regioes de interesse(ROI) uma gray e outra colorida, uma para detecção 
-    # do olho e outra para traçar o retangulo
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = frame[y:y+h, x:x+w]
-    # Detecção do olho com 'haarcascade'
-    eyes = eyes_cascade.detectMultiScale(roi_gray, 1.1, 3, minSize=(25,25))
-    # Desenhando o retangulo ao redor dos olhos
-    for (ex, ey, ew, eh) in eyes:
-       cv2.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh), (0, 255, 0), 2)
       
-  return frame
+  for (x,y,w,h) in faces:
+    cv2.rectangle(image, (x,y), (x+w, y+h), (255,191,0), 2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = image[y:y+h, x:x+w]
     
-# Capturando video da webcam 
-video_capture = cv2.VideoCapture(0)
-# loop infinito 
+    eyes = eyes_cascade.detectMultiScale(roi_gray, 1.1, 20, minSize=(25,25))
+    
+    for (ex, ey, ew, eh) in eyes:
+       cv2.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh), (0, 215, 255), 2)
+       roi2_color = roi_color[ey:ey+eh, ex:ex+ew]
+       roi2_gray = roi_gray[ey:ey+eh, ex:ex+ew]
+       circles = cv2.HoughCircles(roi2_gray, cv2.HOUGH_GRADIENT, 1.3, 70, param1=120, param2=3, minRadius=0, maxRadius=10)
+       if circles is not None:
+        # converte as coordenadas do circulo
+          circles = np.round(circles[0, :]).astype("int")
+     
+        # loop c as coordenadas do circulo
+          for (cx, cy, cr) in circles:
+        #desenha o circulo 
+            cv2.circle(roi2_color, (cx, cy), cr, (50, 205, 50), 2)
+        #marca com um retangulo o centro da circunferencia 
+            cv2.rectangle(roi2_color, (cx - 2, cy - 2), (cx + 2, cy + 2), (47, 255, 173), -1)
+            a,b,c = np.shape(roi2_color)
+            #image[0:a,0:b,0:c] = roi2_color
+      
+  return image
+ 
+# inicia a utilizacao da camera
+picam = PiCamera()
+webcam = VideoStream(src = 0).start()
+time.sleep(2.0)
+picam.resolution = (640, 480)
+picam.framerate = 32
+rawCapture = PiRGBArray(picam, size=(640, 480))
+ 
+# allow the camera to warmup
+time.sleep(0.1)
+ 
 while True:
-  # Lê cada frame
-  _, frame = video_capture.read()
+    # grab the frame_1 from the threaded video stream and resize it
+    # to have a maximum width of 400 pixels
+    #frame_1 = picam.read()
+    frame_2 = webcam.read()
+    #frame_1 = imutils.resize(frame_1, width=400)
+    frame_2 = imutils.resize(frame_2, width=400)
+
+    # draw the timestamp on the frame_1
+    timestamp = datetime.datetime.now()
+    ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
+    #cv2.putText(frame_1, ts, (10, frame_1.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+     #   0.35, (0, 0, 255), 1)
+    cv2.putText(frame_2, ts, (10, frame_2.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+        0.35, (0, 0, 255), 1)
+
+    cv2.imshow("webcam", frame_2)
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("b"):
+        break
+
+# capture frames from the camera
+for frame_1 in picam.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    # representacao da imagem com raw truncate
+    image1 = frame_1.array
+    frame2 = cv2.cvtColor(image1, cv2.COLOR_BGR2YUV)
+    frame2[:,:,0] = cv2.equalizeHist(frame2[:,:,0])
+    image = cv2.cvtColor(frame2, cv2.COLOR_YUV2BGR)
   # Converte o frame para escala de cinza para poder trabalhar no 'haarcascade'
-  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-  # Chama a função de detecção para a imagem cinza e o frame capturado 
-  canvas = detect(gray, frame)
-  # Mostra a imagem 
-  cv2.imshow("Video", canvas)
-  # Coloca uma condição de parada
-  if cv2.waitKey(1) & 0xFF == ord('q'):
-    break
-video_capture.release()
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    canvas = detect(gray, image)
+ 
+    # show the frame
+    cv2.imshow("Frame", image)
+    key = cv2.waitKey(1) & 0xFF
+ 
+    # limpa o stream p proximo frame
+    rawCapture.truncate(0)
+ 
+    # se q é pressionado fecha janela
+    if key == ord("q"):
+        break
 cv2.destroyAllWindows()
+picam.stop()
+webcam.stop()
